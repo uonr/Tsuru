4cce87c83618fe64d2aecf47b08b56c7b14dfe34
Tachibana Kiyomi <me@yuru.me>
2021-04-18 16:13:51 +0900

    Initial Commit

---
+++ Tsuru/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Tsuru
+//
+//  Created by 橘葉月 on 2021/04/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Tsuru/TsuruApp.swift
@@ -0,0 +1,17 @@
+//
+//  TsuruApp.swift
+//  Tsuru
+//
+//  Created by 橘葉月 on 2021/04/18.
+//
+
+import SwiftUI
+
+@main
+struct TsuruApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
