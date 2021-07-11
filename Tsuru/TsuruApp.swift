c227b78957941204fb0f3d54e565a39c0ee3abfa
Tachibana Kiyomi <me@yuru.me>
2021-07-11 11:10:32 +0900

    basic text input

--- Tsuru/TsuruApp.swift
+++ Tsuru/TsuruApp.swift
@@ -8,11 +8,25 @@ var server = IMKServer()
 var candidatesWindow = IMKCandidates()
 
 
+var codeTable: [String: [String]] = [:]
+
 @main
 struct TsuruApp: App {
     init() {
+        
+        let codeTableAsset: NSDataAsset? = NSDataAsset(name: "flypy", bundle: Bundle.main)
+        let codeTableRaw: String = String(decoding: codeTableAsset!.data, as: UTF8.self)
         server = IMKServer(name: connectionName, bundleIdentifier: bundleIdentifier)
         candidatesWindow = IMKCandidates(server: server, panelType: kIMKSingleRowSteppingCandidatePanel, styleType: kIMKMain)
+        codeTableRaw.enumerateLines { line, _ in
+            let splited = line.split(separator: "\t").map { String($0) }
+            if splited.count != 2 {
+                print(line)
+            } else {
+                codeTable[splited[1]] = (codeTable[splited[1]] ?? []) + [splited[0]]
+            }
+        }
+        
     }
     
     var body: some Scene {
--- Tsuru/TsuruInpuController.swift
+++ Tsuru/TsuruInpuController.swift
@@ -28,61 +28,73 @@ class TsuruInputController: IMKInputController {
     private func writeMarkToClient(_ client: Sender,_ string: String) {
         client.setMarkedText(string,
                              selectionRange: NSMakeRange(0, string.count),
-                             replacementRange: NSMakeRange(NSNotFound, NSNotFound))
+                             replacementRange: notFoundRange)
     }
     
-    func showCandidates(client tx: Sender, _ input: String!, _ candidates: [String]) {
-        if (candidates.count == 0) {
-            self._composedString = input
-            commitComposition(tx)
-        } else {
-            self._originalString += input
-            let first = candidates.first!
-            self._composedString += first
-            self._candidates = candidates
-            candidatesWindow.update()
-            candidatesWindow.show()
-            writeMarkToClient(tx, first)
-        }
-    }
-
     override func inputText(_ string: String!, client sender: Any!) -> Bool {
         let tx = castSender(sender)
         switch string {
-        case "h" where !candidatesWindow.isVisible():
-
-            showCandidates(client: tx, string, ["h", "hello"])
-        case "1", "2", "3", "4", "5", "6", "7", "8", "9":
-            let n: Int = Int(string)!
-            if !candidatesWindow.isVisible() || n > self._candidates.count {
-                commitComposition(tx)
-                tx.insertText(string, replacementRange: notFoundRange)
+//        case "1", "2", "3", "4", "5", "6", "7", "8", "9":
+//            let n: Int = Int(string)!
+//            if !candidatesWindow.isVisible() || n > self._candidates.count {
+//                commitComposition(tx)
+//                tx.insertText(string, replacementRange: notFoundRange)
+//            } else {
+//                self._composedString = self._candidates[n - 1]
+//                commitComposition(tx)
+//            }
+        case " ":
+            if candidatesWindow.isVisible() {
+                tx.insertText(self._candidates[0])
+                self._originalString = ""
+                self._composedString = ""
+                self._candidates = []
+                candidatesWindow.hide()
             } else {
-                self._composedString = self._candidates[n - 1]
-                commitComposition(tx)
+                tx.insertText(self._originalString + string)
+                self._originalString = ""
+                self._composedString = ""
+                self._candidates = []
             }
-        case " " where candidatesWindow.isVisible():
-            commitComposition(tx)
         default:
-            commitComposition(tx)
-            tx.insertText(string, replacementRange: notFoundRange)
+            self._originalString += string
+            let match = codeTable[self._originalString] ?? []
+            if match.count == 0 {
+                tx.insertText(self._originalString)
+                self._originalString = ""
+                self._composedString = ""
+                self._candidates = []
+                candidatesWindow.hide()
+            } else {
+                self._candidates = match
+                tx.setMarkedText(self._originalString,
+                                 selectionRange: NSMakeRange(0, self._originalString.count),
+                                 replacementRange: notFoundRange)
+                candidatesWindow.update()
+                candidatesWindow.show()
+            }
+//            self._originalString = ""
+//            self._composedString = ""
+//            self._candidates = []
+//            tx.insertText(string, replacementRange: notFoundRange)
+//            if (match.count > 0) {
+//                showCandidates(client: tx, string, codeTable[self._composedString]!)
+//            } else {
+//                commitComposition(tx)
+//                tx.insertText(string, replacementRange: notFoundRange)
+//            }
+//            let match = codeTable[self._composedString] ?? []
+//            if (codeTable[self._composedString] ?? []).count > 0 && !candidatesWindow.isVisible() {
+//                showCandidates(client: tx, string, codeTable[self._composedString]!)
+//            } else {
+//                commitComposition(tx)
+//                tx.insertText(string, replacementRange: notFoundRange)
+//            }
         }
         return true
     }
     override func candidateSelected(_ candidateString: NSAttributedString!) {
         self._composedString = candidateString.string
         commitComposition(client())
-    }
-    override func commitComposition(_ sender: Any!) {
-        if !self._composedString.isEmpty {
-            let tx = castSender(sender)
-            tx.insertText(self._composedString, replacementRange: notFoundRange)
-        }
-
-        self._originalString = ""
-        self._composedString = ""
-        self._candidates = []
-        
-        candidatesWindow.hide()
     }
 }
