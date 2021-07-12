fc9988830af99a4f2be3e8b95cc4e75b8d5d1672
Tachibana Kiyomi <me@yuru.me>
2021-07-12 20:30:31 +0900

    improve English input

--- Tsuru/TsuruInpuController.swift
+++ Tsuru/TsuruInpuController.swift
@@ -20,12 +20,28 @@ let arrowRight = char(NSRightArrowFunctionKey)
 let backspaceKey = char(NSBackspaceCharacter)
 let enterKey = char(NSEnterCharacter)
 let returnKey = char(NSCarriageReturnCharacter)
-
+let shiftKeyCode: UInt16 = 0x38
+let escapeKeyCode: UInt16 = 0x35
 
 class TsuruInputController: IMKInputController {
     private var inputed: String = "" // what the user typed
-    private var selected: String = ""
-    private var matched: [String] = [] // list of candidates to choose from
+    private var matched: [String] = []
+    private var manuallyRawMode = false
+    override func recognizedEvents(_ sender: Any!) -> Int {
+        // https://stackoverflow.com/a/34245230
+        return Int(NSEvent.EventTypeMask.keyDown.rawValue | NSEvent.EventTypeMask.flagsChanged.rawValue)
+    }
+    private func updateInput(_ next: String) {
+        if next.isEmpty {
+            self.inputed = ""
+            self.matched = []
+            self.manuallyRawMode = false
+            candidatesWindow.hide()
+            return
+        }
+        self.inputed = next
+        self.matched = codeTable[inputed] ?? []
+    }
 
     override func deactivateServer(_ sender: Any!) {
         let client = getClient()
@@ -34,9 +50,7 @@ class TsuruInputController: IMKInputController {
         } else {
             writeMarkToClient(client, "")
         }
-        self.matched = []
-        self.inputed = ""
-        candidatesWindow.hide()
+        updateInput("")
     }
     override func composedString(_ sender: Any!) -> Any! {
         return self.inputed
@@ -50,18 +64,20 @@ class TsuruInputController: IMKInputController {
     override func candidateSelected(_ candidateString: NSAttributedString!) {
         let client = getClient()
         client.insertText(candidateString.string, replacementRange: client.markedRange())
-        self.matched = []
-        self.inputed = ""
-        candidatesWindow.hide()
+        updateInput("")
     }
     override func cancelComposition() {
         let client = getClient()
         writeMarkToClient(client, "")
-        self.matched = []
-        self.inputed = ""
-        candidatesWindow.hide()
+        updateInput("")
     }
     private func isRawMode() -> Bool {
+        if manuallyRawMode {
+            return true
+        }
+        if self.inputed.count >= 4 && self.matched.isEmpty {
+            return true
+        }
         let rawMode = false
         for char in inputed {
             if char.isUppercase {
@@ -87,34 +103,40 @@ class TsuruInputController: IMKInputController {
                              selectionRange: NSMakeRange(0, string.count),
                              replacementRange: replacementRange)
     }
-    override func handle(_ event: NSEvent!, client sender: Any!) -> Bool {
+    override func handle(_ event: NSEvent!, client: Any!) -> Bool {
         switch event.type {
-            case .keyDown: return handleKeyDown(event, getClient(sender))
-            default: return unhandled
+        case .keyDown:
+            return handleKeyDown(event, getClient(client))
+        case .flagsChanged:
+            if event.keyCode == shiftKeyCode && !inputed.isEmpty {
+                manuallyRawMode = true
+                return handled
+            }
+            return unhandled
+        default:
+            return unhandled
         }
     }
     override func updateComposition() {
-        let matched = codeTable[inputed] ?? []
-        NSLog("inputed string: \(inputed) matched: \(matched.joined(separator: ", "))")
-        if (matched.count > 0) {
-            self.matched = matched
-            candidatesWindow.update()
-            candidatesWindow.show()
-        } else {
+        candidatesWindow.update()
+        if (matched.isEmpty) {
             candidatesWindow.hide()
+        } else {
+            candidatesWindow.show()
         }
         let client = getClient()
         writeMarkToClient(client, inputed)
     }
     private func submit(_ client: Client, _ submitted: String) {
-        NSLog("submitted: \(submitted)")
         client.insertText(submitted, replacementRange: client.markedRange())
         candidatesWindow.hide()
-        self.inputed = ""
-        self.matched = []
     }
 
     private func handleKeyDown(_ event: NSEvent, _ client: Client) -> Bool {
+        if event.keyCode == escapeKeyCode && !inputed.isEmpty {
+            cancelComposition()
+            return handled
+        }
         if (event.modifierFlags.contains(.command)) {
             return unhandled
         }
@@ -134,55 +156,70 @@ class TsuruInputController: IMKInputController {
             if inputed.isEmpty {
                 return unhandled
             }
-            self.inputed = String(inputed.dropLast())
+            updateInput(String(inputed.dropLast()))
 
             if inputed.isEmpty {
-                self.cancelComposition()
+                cancelComposition()
             } else {
-                self.updateComposition()
+                updateComposition()
             }
             return handled
         case " ":
-            if inputed.isEmpty || matched.isEmpty || isRawMode() {
-                inputed = ""
-                matched = []
+            let matched = self.matched
+            if isRawMode() {
+                updateInput(inputed + " ")
+                updateComposition()
+                return handled
+            } else if inputed.isEmpty || matched.isEmpty {
+                updateInput("")
                 return unhandled
             }
             submit(client, matched[0])
+            updateInput("")
             return handled
         case enterKey, returnKey:
             if inputed.isEmpty {
                 return unhandled
             }
             submit(client, inputed)
+            updateInput("")
             return handled
         case ";":
-            if inputed.isEmpty && self.matched.count < 2 {
+            let matched = self.matched
+            if matched.count >= 2 {
+                submit(client, matched[1])
+                updateInput("")
+                return handled
+            } else if inputed.isEmpty {
+                return unhandled
+            } else if matched.isEmpty {
+                submit(client, inputed)
+                updateInput("")
+                return unhandled
+            } else {
+                submit(client, matched[0])
+                updateInput("")
                 return unhandled
             }
-            submit(client, matched[1])
-            return handled
-        case ".":
+        case "." where !isRawMode():
             submit(client, "。")
             return handled
-        case ",":
+        case "," where !isRawMode():
             submit(client, "，")
             return handled
         default:
             let maxCode = 4
+            let matched = matched
             if !char.isLetter {
                 submit(client, inputed)
+                updateInput("")
                 return unhandled
             }
-            
-            if inputed.count == maxCode && !isRawMode() {
-                if matched.isEmpty {
-                    submit(client, inputed)
-                } else {
-                    submit(client, matched[0])
-                }
+            if inputed.count == maxCode && !isRawMode() && !matched.isEmpty {
+                submit(client, matched[0])
+                updateInput("")
             }
-            self.inputed = inputed + String(char)
+            updateInput(inputed + String(char))
             self.updateComposition()
             return handled
         }
